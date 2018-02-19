7437a666015674cc1418b5d197311f42c2f69b7c
tuhuynhkhtn <tuhuynhkhtn@gmail.com>
2018-02-20 04:21:18 +0700

    LoadRoom
    LoadRoom

--- MVCQLKS/MVCQLKS/Controllers/RoomController.cs
+++ MVCQLKS/MVCQLKS/Controllers/RoomController.cs
@@ -7,15 +7,15 @@ using System.Web.Mvc;
 
 namespace MVCQLKS.Controllers
 {
-    public class CategoryController : Controller
+    public class RoomController : Controller
     {
-        // GET: Category
-        public ActionResult GetList()
+        // GET: Room
+        public ActionResult GetListByCategory(int catId)
         {
             using (var dc = new QLKSEntities())
             {
-                var l = dc.Categories.ToList();
-                return PartialView("_PartialList", l);
+                var l = dc.Rooms.Where(p => p.CatID == catId).ToList();
+                return View("ListByCategory", l);
             }
         }
     }
