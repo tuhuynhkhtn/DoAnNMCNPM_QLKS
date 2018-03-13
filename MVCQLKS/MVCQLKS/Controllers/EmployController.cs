4a05af11c6e91794a22370a27458c4aa64de6e57
tuhuynhkhtn <tuhuynhkhtn@gmail.com>
2018-03-14 03:21:25 +0700

    Thêm Controller Employ cùng 1 số action
    Thêm Controller Employ cùng 1 số action

---
+++ MVCQLKS/MVCQLKS/Controllers/EmployController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCQLKS.Models;
+
+namespace MVCQLKS.Views
+{
+    public class EmployController : Controller
+    {
+        //// GET: Employ
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: ManageRoom/QuanLyRoomChoNV
+        public ActionResult QuanLyRoomChoNV()
+        {
+            using (var dc = new QLKSEntities())
+            {
+                var l = dc.Rooms.ToList();
+                return View(l);
+            }
+        }
+
+        // GET: ManageCategory/QuanLyCatChoNV
+        public ActionResult QuanLyCatChoNV()
+        {
+            using (var dc = new QLKSEntities())
+            {
+                var l = dc.Categories.ToList();
+                return View(l);
+            }
+        }
+
+    }
+}
--- MVCQLKS/MVCQLKS/Controllers/ManageRoomController.cs
+++ MVCQLKS/MVCQLKS/Controllers/ManageRoomController.cs
@@ -15,25 +15,25 @@ namespace MVCQLKS.Controllers
             return View();
         }
 
-        // GET: ManageRoom/QuanLyRoom
-        public ActionResult QuanLyRoom()
-        {
-            using (var dc = new QLKSEntities())
-            {
-                var l = dc.Rooms.ToList();
-                return View(l);
-            }
-        }
+        //// GET: ManageRoom/QuanLyRoom
+        //public ActionResult QuanLyRoom()
+        //{
+        //    using (var dc = new QLKSEntities())
+        //    {
+        //        var l = dc.Rooms.ToList();
+        //        return View(l);
+        //    }
+        //}
 
-        // GET: ManageCategory/QuanLyCat
-        public ActionResult QuanLyCat()
-        {
-            using (var dc = new QLKSEntities())
-            {
-                var l = dc.Categories.ToList();
-                return View(l);
-            }
-        }
+        //// GET: ManageCategory/QuanLyCat
+        //public ActionResult QuanLyCat()
+        //{
+        //    using (var dc = new QLKSEntities())
+        //    {
+        //        var l = dc.Categories.ToList();
+        //        return View(l);
+        //    }
+        //}
 
     }
 }
